9dc3d724681359cc33f03d27d9e74b954174e95d
Logan Goodwin <lgoodwin4680@gmail.com>
2025-06-24 16:24:22 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -2,18 +2,27 @@ import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
+// This hook is designed to be used to determine if the user is on a mobile device.
+// It is safe to use in server components, as it will default to `false` on the server
+// and only update on the client after mounting. This prevents hydration mismatches.
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
+  const [hasMounted, setHasMounted] = React.useState(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
+    setHasMounted(true)
+    
+    const checkDevice = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    checkDevice()
+    window.addEventListener("resize", checkDevice)
+
+    return () => {
+      window.removeEventListener("resize", checkDevice)
+    }
   }, [])
 
-  return !!isMobile
+  return hasMounted && isMobile
 }
